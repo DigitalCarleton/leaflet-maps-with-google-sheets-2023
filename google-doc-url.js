e2148ec18fc2cf407372b55277f7afb183175b2c
Austin Mason <austin.pj.mason@gmail.com>
2023-07-09 12:06:14 -0500

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1mos9ML7dYK-6gwWHu2cAkLvlGC6QG-9Ley_YU-_ar1s/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/11hO-oTgKBE_DMOcYED6W0h3xatONiMSO81hS7Iwn4es/edit#gid=164271551';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
